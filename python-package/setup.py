2abf9facab2952177685749ae5b527f380b9c4ae
Icyblade Dai <icyblade.py@gmail.com>
2017-11-16 10:13:36 +0800

    minor fix

--- python-package/setup.py
+++ python-package/setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='eva',
     version='0.0.1',
-    packages=['eva'],
+    packages=['eva', 'eva.dataframe', 'eva.frontend', 'eva.hardware', 'eva.hotel', 'eva.model', 'eva.ota', 'eva.pms'],
     url='https://github.com/icyblade/eva',
     license='MIT',
     author='Icyblade Dai',
     author_email='icyblade.aspx@gmail.com',
-    description='Project Eva',
+    description='Project EVA',
     classifiers=[
         'Programming Language :: Python :: 3.6',
     ],
